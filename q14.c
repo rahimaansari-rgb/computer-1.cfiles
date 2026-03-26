f698f7fbafb91e27846bf366a0f99921110c59b1
rahimaansari-rgb <rahima.ansari@auw.edu.bd>
2026-03-26 06:47:52 -0700

    Add files via upload

---
+++ q14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int i, n;
+
+    n = get_int("enter n: ");
+
+    for(i = 1; i <= n; i++)
+    {
+        if(i % 2 != 0)
+        {
+            printf("%d ", i);
+        }
+    }
+}
