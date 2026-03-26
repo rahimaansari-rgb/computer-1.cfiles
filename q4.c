8736ce5ff850268d5702107d3cb28ea9670f5411
rahimaansari-rgb <rahima.ansari@auw.edu.bd>
2026-03-26 06:41:44 -0700

    Add files via upload

---
+++ q4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 5;
+
+    printf("initial value: %d\n", a);
+
+    a++;
+    printf("after increment: %d\n", a);
+
+    a--;
+    printf("after decrement: %d\n", a);
+}
