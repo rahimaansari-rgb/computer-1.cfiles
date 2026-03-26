971ff483d442b761732394d3703bf25d41fa823e
rahimaansari-rgb <rahima.ansari@auw.edu.bd>
2026-03-26 06:50:47 -0700

    Add files via upload

---
+++ q19.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int i, n;
+
+    n = get_int("enter number: ");
+
+    for(i = 1; i <= 5; i++)
+    {
+        printf("%d ", n*i);
+    }
+
+    printf("\n");
+}
